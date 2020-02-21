0dbafb1c107245cde43f14a9ed32e412d3b71919
Rkozuma <rkozuma@hawaii.edu>
2020-02-20 23:25:53 -1000

    Error Occurred - SIGBART

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     // I used a label because there were no display text views...
     @IBOutlet weak var UniDes: UILabel!
     
-    
+ 
     override func viewDidLoad()
     {
         // DISPLAY ATTRIBUTES
@@ -25,21 +25,21 @@ class ViewController: UIViewController {
         // TEXT ATTRIBUTES
         self.FirstLabel.text = "University of Hawai`i West O`ahu"
         self.UniDes.text = "A four-year, indigenous-serving institution, UH West Oʻahu provides an intellectually challenging higher education experience in a supportive setting, offering more than 30 academic concentrations in six degree programs designed for career success."
-        
-    
-        
-
-        
 
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
     @IBAction func AboutACM(_ sender: Any)
-     {
-     }
+    {
+        
+    }
     
+    @IBAction func AboutUHWO(_ sender: Any)
+    {
+        
+    }
     
-
 }
 
