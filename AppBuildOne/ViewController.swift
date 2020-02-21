6650da625ae5066ff3f7ed3e043165a07ea33608
Rkozuma <rkozuma@hawaii.edu>
2020-02-20 22:56:24 -1000

    Task for first view controller: Steps 1 - 3

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var FirstLabel: UILabel!
+    // I used a label because there were no display text views...
+    @IBOutlet weak var UniDes: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
+        // DISPLAY ATTRIBUTES
+        self.FirstLabel.textColor = UIColor.white
+        self.UniDes.textColor = UIColor.white
+        self.view.backgroundColor = UIColor.gray
+        
+        // TEXT ATTRIBUTES
+        self.FirstLabel.text = "University of Hawai`i West O`ahu"
+        self.UniDes.text = "A four-year, indigenous-serving institution, UH West Oʻahu provides an intellectually challenging higher education experience in a supportive setting, offering more than 30 academic concentrations in six degree programs designed for career success."
+        
+    
+        
+
+        
+
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func AboutACM(_ sender: Any)
+     {
+     }
+    
+    
 
 }
 
